87aadf7bdb3f31315e4ffd328a88e0b39d605313
Alex Dupre <ale@FreeBSD.org>
2018-06-18 16:02:06 +0200

    CoinInfo status is optional.

--- common/src/main/scala/com/alexdupre/shapeshift/models/CoinInfo.scala
+++ common/src/main/scala/com/alexdupre/shapeshift/models/CoinInfo.scala
@@ -3,18 +3,20 @@ package com.alexdupre.shapeshift.models
 import com.alexdupre.shapeshift.models.Availability._
 import play.api.libs.json.Json
 
-case class CoinInfo(name: String,
-                    symbol: Coin,
-                    image: String,
-                    imageSmall: String,
-                    status: Availability,
-                    specialReturn: Option[Boolean],
-                    specialOutgoing: Option[Boolean],
-                    specialIncoming: Option[Boolean],
-                    specialIncomingStatus: Option[Boolean],
-                    fieldName: Option[String],
-                    fieldKey: Option[String],
-                    qrName: Option[String])
+case class CoinInfo(
+    name: String,
+    symbol: Coin,
+    image: String,
+    imageSmall: Option[String],
+    status: Option[Availability],
+    specialReturn: Option[Boolean],
+    specialOutgoing: Option[Boolean],
+    specialIncoming: Option[Boolean],
+    specialIncomingStatus: Option[Boolean],
+    fieldName: Option[String],
+    fieldKey: Option[String],
+    qrName: Option[String]
+)
 
 object CoinInfo {
 
